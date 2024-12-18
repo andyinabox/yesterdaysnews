3d6b581b696d70c92c2d22b4310b1fb1589adfe3
Andy Dayton <amdayton@gmail.com>
2024-12-18 18:03:04 +0100

    some cleanup

--- pkg/mediatool/combine-videos.go
+++ pkg/mediatool/combine-videos.go
@@ -10,7 +10,7 @@ import (
 	"gitlab.com/andyinabox/yesterdays-news-downloader/pkg/shellargs"
 )
 
-const VideoListFileName = ".file_list.txt"
+const VideoListFileName = ".file-list.txt"
 
 func (t *Tool) CombineVideos(ctx context.Context, files []string, outFile string) (file string, err error) {
 
@@ -21,29 +21,12 @@ func (t *Tool) CombineVideos(ctx context.Context, files []string, outFile string
 		return
 	}
 
-	// tmp, err := os.CreateTemp("", "videos.*.txt")
-	// if err != nil {
-	// 	return "", err
-	// }
-	// defer os.Remove(tmp.Name())
-
 	// generate file with filenames
-	fileList := ""
+	var fileList string
 	for _, f := range files {
-		// abs, err := filepath.Abs(f)
-		// if err != nil {
-		// 	return "", fmt.Errorf("cannot find absolute path for %s, %w", f, err)
-		// }
+		// ffmpeg format is "file 'path/to/file.mp4'"
 		fileList = fileList + fmt.Sprintf("file '%s'\n", f)
 	}
-
-	// log.Debug(fileList)
-
-	// _, err = tmp.Write([]byte(fileList))
-	// if err != nil {
-	// 	return "", fmt.Errorf("error writing temp file: %w", err)
-	// }
-	// tmp.Close()
 
 	err = os.WriteFile(VideoListFileName, []byte(fileList), os.ModePerm)
 	defer os.Remove(VideoListFileName)
