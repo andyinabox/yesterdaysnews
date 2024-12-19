f72393f6b68b8cf58df7e47c8e72e8a4762731c5
Andy Dayton <amdayton@gmail.com>
2024-12-19 14:05:38 +0100

    stub out caption generator

---
+++ domain/textprocessor/generate-captions.go
@@ -0,0 +1,10 @@
+package textprocessor
+
+import (
+	"errors"
+	"time"
+)
+
+func (p *Processor) GenerateCaptions(videoLength time.Duration) ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
