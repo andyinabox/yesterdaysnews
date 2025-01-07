e1570ca8c69ecc448412855a3e25e429a0d0b9a0
Andy Dayton <amdayton@gmail.com>
2025-01-07 09:31:55 -0500

    wip

---
+++ domain/builder/config.go
@@ -0,0 +1,56 @@
+package builder
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
+)
+
+const (
+	envTagName = "env"
+)
+
+type Config struct {
+	// env variables
+	GoogleAPIKey string `env:"GOOGLE_API_KEY"`
+	S3Endpoint   string `env:"YN_S3_ENDPOINT"`
+	S3AccessKey  string `env:"YN_S3_ACCESS_KEY"`
+	S3SecretKey  string `env:"YN_S3_SECRET_ACCESS_KEY"`
+
+	// config variables
+	ObjectStoreContainerName string
+	ObjectStorePrimaryDir    string
+	DownloadDir              string
+	OutputDir                string
+	ClipsDirName             string
+	MinClipLengthSeconds     int
+	MaxClipLengthSeconds     int
+}
+
+func (c *Config) Load(configFile string) error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Warnf("error loading .env: %s", err)
+	}
+
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return fmt.Errorf("error reading config file: %w", err)
+	}
+
+	err = json.Unmarshal(data, c)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling config file: %w", err)
+	}
+
+	// doing this until I get struct tag working
+	c.GoogleAPIKey = os.Getenv("GOOGLE_API_KEY")
+	c.S3Endpoint = os.Getenv("YN_S3_ENDPOINT")
+	c.S3AccessKey = os.Getenv("YN_S3_ACCESS_KEY")
+	c.S3SecretKey = os.Getenv("YN_S3_SECRET_ACCESS_KEY")
+
+	return nil
+}
