adb79df3bec42930d2991d09c74f94101b3c5b81
Andy Dayton <amdayton@gmail.com>
2025-01-14 13:48:47 +0100

    fix unused code

--- pkg/util/string-slice-remove.go
+++
@@ -1,6 +0,0 @@
-package util
-
-// StringSliceRemove will return a slice with the given element removed
-func StringSliceSplice(slice []string, i int) (string, []string) {
-	return slice[i], append(slice[:i], slice[i+1:]...)
-}
