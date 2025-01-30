41f1929219d18a3a68ef96fef3f73ffe758971ce
Andy Dayton <amdayton@gmail.com>
2025-01-30 09:54:53 +0100

    a little better but i still dont love it

--- app/server/assets/main.js
+++ app/server/assets/main.js
@@ -2,11 +2,11 @@ import { EventButton } from '/assets/event-button.js'
 import { EventIcon } from '/assets/event-icon.js'
 import { CaptionDisplay } from '/assets/caption-display.js'
 import { VideoPlayer } from '/assets/video-player.js'
-import { OrientationDialog } from '/assets/orientation-dialog.js'
 
 // events
 const ABOUT_OPEN_EVENT = 'yn-open-about'
 const ABOUT_CLOSE_EVENT = 'yn-close-about'
+const ORIENTATION_CLOSE_EVENT = 'yn-close-orientation'
 const FULLSCREEN_ENTER_EVENT = 'yn-enter-fullscreen'
 
 // classes
@@ -15,18 +15,38 @@ const FULLSCREEN_BODY_CLASS = 'fullscreen'
 // ids
 const MAIN_VIEWER_ID = 'main'
 const ABOUT_MODAL_ID = 'about'
+const ORIENTATION_MODAL_ID = 'orientation'
 
 function registerComponents() {
   EventButton.register()
   EventIcon.register()
   CaptionDisplay.register()
   VideoPlayer.register()
-  OrientationDialog.register()
+}
+
+function isFullScreen() {
+  return document.body.classList.contains(FULLSCREEN_BODY_CLASS)
+}
+
+function checkOrientation() {
+  const orientationModal = document.getElementById(ORIENTATION_MODAL_ID)
+
+  if (!isFullScreen()) {
+    orientationModal.close()
+    return
+  }
+
+  if (screen.orientation.type.includes('portrait')) {
+    orientationModal.showModal()
+  } else {
+    orientationModal.close()
+  }
 }
 
 function registerEventListeners() {
   const mainViewer = document.getElementById(MAIN_VIEWER_ID)
   const aboutModal = document.getElementById(ABOUT_MODAL_ID)
+  const orientationModal = document.getElementById(ORIENTATION_MODAL_ID)
 
   document.addEventListener(ABOUT_OPEN_EVENT, () => {
     aboutModal.showModal()
@@ -40,11 +60,18 @@ function registerEventListeners() {
     mainViewer.requestFullscreen()
   })
 
+  document.addEventListener(ORIENTATION_CLOSE_EVENT, () => {
+    orientationModal.close()
+  })
+
   document.addEventListener('fullscreenchange', () => {
     if (!!document.fullscreenElement) {
       document.body.classList.add(FULLSCREEN_BODY_CLASS)
+      checkOrientation()
+      // we only want to use the dialog in full screen mode
     } else {
       document.body.classList.remove(FULLSCREEN_BODY_CLASS)
+      checkOrientation()
     }
   })
 
@@ -59,6 +86,10 @@ function registerEventListeners() {
       }
     }
   })
+
+  // orientation events
+  screen.orientation.addEventListener('change', checkOrientation)
+  checkOrientation()
 }
 
 function registerReloadEventSource() {
