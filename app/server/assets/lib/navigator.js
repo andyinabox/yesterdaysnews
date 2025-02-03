4e0ee0bb1335a53c5dc184855168f0f4f7908067
Andy Dayton <amdayton@gmail.com>
2025-02-03 17:55:55 +0100

    remove console, add comment

--- app/server/assets/lib/navigator.js
+++ app/server/assets/lib/navigator.js
@@ -2,18 +2,17 @@ export function canRequestFullscreen(el) {
   return !!el.requestFullscreen
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide
 export function canAutoplayVideoIfMuted(videoEl) {
+  // in this case we have no way of knowing so assume no
   if (!navigator.getAutoplayPolicy) {
     return false
   }
 
+  // https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide
   const policy = navigator.getAutoplayPolicy(videoEl)
   if (!policy) {
     return false
   }
-
-  console.log('policy', policy)
 
   return policy === 'allowed' || policy === 'allowed-muted'
 }
