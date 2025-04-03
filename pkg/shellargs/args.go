e8bb0435697e4af26c4d957406eae6d2ec2b323e
Andy Dayton <amdayton@gmail.com>
2025-04-03 17:50:07 +0200

    merge in shellargs fix

--- pkg/shellargs/args.go
+++ pkg/shellargs/args.go
@@ -26,10 +26,6 @@ func (a *Args) AddKeyedSingleQuoted(key, value string) *Args {
 }
 
 func (a *Args) String() string {
-<<<<<<< HEAD
-	// fmt.Print(len(a.args))
-=======
->>>>>>> main
 	output := ""
 	for _, values := range a.args {
 		if len(values) == 2 {
